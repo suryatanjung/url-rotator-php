d44dfc335407226e9932ff69441ce0680973a983
jung <70964278+suryatanjung@users.noreply.github.com>
2023-11-13 16:10:51 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$urls = [
+    'URL1',
+    'URL2',
+    'URL3',
+    'URL4',
+]; 
+
+// Data file 
+$dataFile = './count.txt';
+
+// Read the data file and parse it into an array
+$index = file_exists($dataFile) ? file_get_contents($dataFile) : -1;
+if($index + 1 >= count($urls)){
+    $index = -1;
+}
+$index = $index + 1;
+
+// Sve document
+file_put_contents($dataFile, $index);
+
+// Visit the corresponding URL
+$url = $urls[$index];
+// echo $url;
+header("Refresh: 0.1;url=https://$url");
+exit();
